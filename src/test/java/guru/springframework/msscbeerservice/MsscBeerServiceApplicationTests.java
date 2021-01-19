303b04e1704ba7447c74bdbda232a0139af0ef79
sindhusha <sindhushajeeru@gmail.com>
2021-01-19 17:55:23 +0100

    Adding controller tests

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -3,6 +3,8 @@ package guru.springframework.msscbeerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
