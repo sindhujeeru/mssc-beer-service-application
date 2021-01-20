eeca2b2384463dc57a390c51f3a210477bee61de
sindhusha <sindhushajeeru@gmail.com>
2021-01-20 16:04:15 +0100

    Adding error handling class

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex){
+        List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
+        ex.getConstraintViolations().forEach(error->
+                errorsList.add(error.toString()));
+        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+    }
+}
